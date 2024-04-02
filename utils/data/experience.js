f573fd4afe58047dbe1275a4cb126cfdeb038ab2
andersonbarbosadev <andersonbarbosadev@outlook.com>
2024-04-02 16:56:26 -0500

    add skills and about

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,41 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
+    title: "Software Engineer I",
     company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    duration: "(Jan 2022 - Present)",
   },
   {
     id: 2,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    duration: "(Jun 2021 - Jan 2022)",
   },
   {
     id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
-]
+    duration: "(Jan 2018 - Present)",
+  },
+];
+
+export const softSkills = [
+  {
+    id: 1,
+    title: "Liderazgo de Equipos",
+    description:
+      "Escuchar y valorar a cada miembro del equipo es fundamental para su motivación y el éxito conjunto. Liderar es guiar con el ejemplo, apoyar y aprender de todos a tu alrededor.",
+  },
+  {
+    id: 2,
+    title: "Planificación Estratégica    ",
+    description:
+      "Anticipar tendencias, establecer objetivos claros y adaptarse rápidamente son claves para el éxito sostenido.    ",
+  },
+  {
+    id: 3,
+    title: "Gestión de Proyectos    ",
+    description:
+      "La gestión de proyectos eficaz se centra en definir objetivos claros, priorizar tareas y mantener una comunicación fluida, asegurando que el equipo avance unido hacia metas compartidas.",
+  },
+];
