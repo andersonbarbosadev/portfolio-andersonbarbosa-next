0c5c62bb97471edfe4ed16bd21d31b7816f20af6
andersonbarbosadev <andersonbarbosadev@outlook.com>
2024-04-10 10:20:19 -0500

    delete next-intl

---
+++ src/app/components/popupLang.jsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState, useTransition } from "react";
+// @flow strict
+import Image from "next/image";
+import FlagEn from "/public/image/FlagEn.svg";
+import FlagEs from "/public/image/FlagEs.svg";
+import { useRouter } from "next/navigation";
+
+function PopupLang() {
+  const langs = [
+    {
+      label: "Ingles",
+      value: "en",
+      icon: FlagEn,
+    },
+    {
+      label: "Español",
+      value: "es",
+      icon: FlagEs,
+    },
+  ];
+
+  const [open, setOpen] = useState(false);
+
+  const router = useRouter();
+
+  const handlePopup = () => {
+    setOpen(!open);
+    const popup = document.getElementById("popup");
+    popup.classList.toggle("show");
+  };
+
+  const changeLang = (lang) => {
+    console.log(lang);
+    // router.replace(`/${lang}`);
+  };
+
+  return (
+    <li>
+      <div
+        className="block px-4 py-2 no-underline outline-none hover:no-underline popup hover:text-pink-600"
+        onClick={handlePopup}
+      >
+        Idioma
+        <span
+          className="text-sm text-white transition-colors duration-300 popuptext"
+          id="popup"
+        >
+          {langs.map((lang) => (
+            <div key={lang.value}>
+              <span
+                className="text-sm text-white transition-colors duration-300 hover:text-pink-600 inline-flex items-center gap-1 text-start"
+                onClick={() => changeLang(lang.value)}
+              >
+                <Image
+                  src={lang.icon}
+                  alt={lang.label}
+                  width={20}
+                  height={20}
+                />
+                {lang.label}
+              </span>
+            </div>
+          ))}
+        </span>
+      </div>
+    </li>
+  );
+}
+
+export default PopupLang;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,11 @@
 import { GoogleTagManager } from "@next/third-parties/google";
-import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+
 import "./css/card.scss";
 import "./css/globals.scss";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Portafolio de Anderson Barbosa",
@@ -14,10 +13,11 @@ export const metadata = {
     "Fullstack developer con más de 7 años de experiencia en la creación de aplicaciones moviles y web.",
 };
 
-export default function RootLayout({ children }) {
+// Receive messages provided in `i18n.ts`
+export default function RootLayout({ children, params: { locale } }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang={locale}>
+      <body>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
