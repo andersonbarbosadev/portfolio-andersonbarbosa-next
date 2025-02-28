e03ed0604c04a0d3a0ecdef1f59798ead8bfef2c
Anderson Barbosa <andersonbarbosadev@outlook.com>
2025-02-28 13:20:28 +0100

    feat: :goal_net: Added sentry in the project to monitor errors

---
+++ sentry.server.config.js
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs"
+
+Sentry.init({
+  dsn: "https://edd7c9295496d218dd16e8744211903a@o4508896720650240.ingest.us.sentry.io/4508896723730432",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
